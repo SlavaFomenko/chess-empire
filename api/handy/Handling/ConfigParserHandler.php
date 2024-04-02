549eb1714f6a7591eb3d8f31b42304e062ebf0eb
Slava_Fomenko <115363889+SlavaFomenko@users.noreply.github.com>
2024-04-02 22:51:05 +0300

    code cleanup

--- api/handy/Handling/ConfigParserHandler.php
+++ api/handy/Handling/ConfigParserHandler.php
@@ -4,14 +4,11 @@ namespace Handy\Handling;
 
 use Handy\Config\ConfigParser;
 use Handy\Context;
-use Handy\Handling\AbstractHandler;
 
 class ConfigParserHandler extends AbstractHandler
 {
     public function handle(Context $ctx): void
     {
-//        ConfigParser::parseUserConfig($ctx);
-
         ConfigParser::parseConfig($ctx);
         parent::handle($ctx);
     }
