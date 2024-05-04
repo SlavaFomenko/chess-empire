208d7d68708908240b52c1598cebb5476431c13c
Slava_Fomenko <115363889+SlavaFomenko@users.noreply.github.com>
2024-05-04 14:05:50 +0300

    dev navigation menu

--- frontend/src/widgets/side-bar/ui/side-bar.jsx
+++ frontend/src/widgets/side-bar/ui/side-bar.jsx
@@ -1,10 +1,30 @@
 import React from "react";
-import styles from '../styles/side-bar.module.scss'
+import { useLocation, useNavigate } from "react-router-dom";
+import styles from "../styles/side-bar.module.scss";
 
 export function SideBar () {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const redirect = (page) => {
+    if(location.pathname !== page){
+      navigate(page);
+    }
+  };
+
   return (
     <div className={styles.side_bar}>
-      im side bar
+      <ul>
+        <li>
+          <button onClick={() => redirect("/")}>home page</button>
+        </li>
+        <li>
+          <button onClick={() => redirect("/game")}>game page</button>
+        </li>
+        <li>
+          <button onClick={() => redirect("/authorization")}>authorization page</button>
+        </li>
+      </ul>
     </div>
   );
 }
