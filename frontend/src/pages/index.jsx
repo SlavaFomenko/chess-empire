4f3a9c6b24c56364f40f3d214cc80ed94183e494
Slava_Fomenko <115363889+SlavaFomenko@users.noreply.github.com>
2024-05-18 13:18:40 +0300

    fixed render bug

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Routes, Route } from "react-router";
 import { GamePage }  from "./game";
 import { AuthorizationPage } from "./sign-in/authorization";
@@ -5,14 +6,21 @@ import { RegestrationPage } from "./sign-in/registration";
 import { HomePage } from "./home";
 import { NotFoundPage } from "./page-404";
 
-export const Routing = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/game" element={<GamePage />} />
-      <Route path="/authorization" element={<AuthorizationPage />} />
-      <Route path="/regestration" element={<RegestrationPage />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
-  );
-};
+export class Routing extends React.Component {
+
+  shouldComponentUpdate () {
+    return false;
+  }
+
+  render() {
+    return (
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/game" element={<GamePage />} />
+        <Route path="/authorization" element={<AuthorizationPage />} />
+        <Route path="/regestration" element={<RegestrationPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    );
+  }
+}
